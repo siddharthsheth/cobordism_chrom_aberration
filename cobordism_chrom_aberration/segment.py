dc3457596fdd5d7591f2d65c3e76e92843caa116
Siddharth Sheth <siddharthsheth@Siddharths-MacBook-Air.local>
2023-05-24 12:21:46 -0400

    add from_string to segment.py

--- cobordism_chrom_aberration/segment.py
+++ cobordism_chrom_aberration/segment.py
@@ -13,6 +13,13 @@ class Label:
     
     def __eq__(self,other):
         return True if self.name==other.name else False
+    
+    @staticmethod
+    def from_string(str):
+        if str[0] == '_' and str[-1] == '_':
+            return Label(str[1:-2], True)
+        else:
+            return Label(str, False)
 
 class Chromatin:
     def __init__(self, start:Label, end:Label, color:str, centromere:bool=False):
@@ -29,13 +36,43 @@ class Chromatin:
     
     def __str__(self):
         return f'{self.start}*{self.color.upper()}*{self.end}' if self.centromere is True else f'{self.start}*{self.color}*{self.end}'
+    
+    @staticmethod
+    def from_string(str):
+        start, color, end = str.split('*')
+        start = Label.from_string(start)
+        end = Label.from_string(end)
+        if color.isupper():
+            return Chromatin(start, end, color.lower(), True)
+        else:
+            return Chromatin(start, end, color)
 
 # Chromatin = namedtuple('Chromatin', ['start', 'end', 'color', 'centromere'], defaults=[False])
+class Interval:
+    def __init__(self, start, end, next_segment=None, prev_segment=None):
+        self.labels = [start, end]
+        self.next_segment = next_segment
+        self.prev_segment = prev_segment
+        pass
+
 
 class ChromSegment:
-    def __init__(self, chromatins):
-        self.chromatins = chromatins
+    def __init__(self, chromatins, next_segment=None, prev_segment=None):
+        if isinstance(chromatins, Chromatin):
+            self.labeled = False
+            self.chromatins = chromatins
+        elif hasattr(chromatins, '__iter__'):
+            self.labeled = True
+            self.chromatins = [c for c in chromatins]
+        self.next_segment = next_segment
+        self.prev_segment = prev_segment
 
+    @staticmethod
+    def from_string(str):
+        chromatins = str.split('--')
+        return ChromSegment([Chromatin.from_string(c) for c in chromatins]) if len(chromatins) > 1 else ChromSegment(Chromatin())
+        
+    
     """
     1. telomere end cannot be connected to any free chromatin end in the segment.
     2. there cannot be duplicate centromeres of the same color in the segment.
